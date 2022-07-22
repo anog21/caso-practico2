258ac61f76dc4ff4772ea974adb7bcb6c57cb0ab
anog21 <106991230+anog21@users.noreply.github.com>
2022-07-23 01:50:37 +0200

    Add files via upload

---
+++ terraform/launch-terraform.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+terraform init
+terraform validate
+terraform plan -var-file="./vars.tfvars"
+terraform apply -var-file="./vars.tfvars"
+echo '---------------------------------------------'
+echo 'Master public IP:'
+terraform output public_ip_address_master
+MASTERIP=$(terraform output public_ip_address_master | sed 's/\"//g')
+echo 'master.azure'
+echo '---------------------------------------------'
+echo 'Workers public IPs:'
+terraform output public_ip_address_worker
+WORKERIP=$(terraform output public_ip_address_worker | sed 's/\"//g')
+echo 'worker1.azure'
+terraform output public_ip_address_worker2
+WORKERIP2=$(terraform output public_ip_address_worker2 | sed 's/\"//g')
+echo 'worker2.azure'
+echo '---------------------------------------------'
+echo 'NFS public IP:'
+terraform output public_ip_address_nfs
+NFSIP=$(terraform output public_ip_address_nfs | sed 's/\"//g')
+echo 'nfs.azure'
+echo '---------------------------------------------'
+sed -i '' "6s/.*/$MASTERIP/" ../ansible/hosts
+sed -i '' "9s/.*/$WORKERIP/" ../ansible/hosts
+sed -i '' "10s/.*/$WORKERIP2/" ../ansible/hosts
+sed -i '' "13s/.*/$NFSIP/" ../ansible/hosts
---
+++ terraform/network.tf
@@ -0,0 +1,139 @@
+
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/virtual_network
+# Especificamos la red virtual para las VMs
+resource "azurerm_virtual_network" "myNet" {
+    name                = var.virtual_network_name
+    address_space       = ["10.0.0.0/24"]
+    location            = azurerm_resource_group.rg.location
+    resource_group_name = azurerm_resource_group.rg.name
+    tags = {
+        environment = "CasoPractico2"
+    }
+}
+
+# Creación de subnet
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/subnet
+
+resource "azurerm_subnet" "mySubnet" {
+    name                   = var.virtual_subnet_name
+    resource_group_name    = azurerm_resource_group.rg.name
+    virtual_network_name   = azurerm_virtual_network.myNet.name
+    address_prefixes       = ["10.0.0.0/24"]
+}
+resource "azurerm_public_ip" "publicIP1" {
+    name                = "vmip1"
+    location            = azurerm_resource_group.rg.location
+    resource_group_name = azurerm_resource_group.rg.name
+    allocation_method   = "Dynamic"
+    sku                 = "Basic"
+
+    tags = {
+        environment = "CasoPractico2"
+    }
+
+}
+resource "azurerm_public_ip" "publicIP2" {
+    name                = "vmip2"
+    location            = azurerm_resource_group.rg.location
+    resource_group_name = azurerm_resource_group.rg.name
+    allocation_method   = "Dynamic"
+    sku                 = "Basic"
+
+    tags = {
+        environment = "CasoPractico2"
+    }
+
+}
+resource "azurerm_public_ip" "publicIP3" {
+    name                = "vmip3"
+    location            = azurerm_resource_group.rg.location
+    resource_group_name = azurerm_resource_group.rg.name
+    allocation_method   = "Dynamic"
+    sku                 = "Basic"
+
+    tags = {
+        environment = "CasoPractico2"
+    }
+
+}
+resource "azurerm_public_ip" "publicIP4" {
+    name                = "vmip4"
+    location            = azurerm_resource_group.rg.location
+    resource_group_name = azurerm_resource_group.rg.name
+    allocation_method   = "Dynamic"
+    sku                 = "Basic"
+
+    tags = {
+        environment = "CasoPractico2"
+    }
+
+}
+
+resource "azurerm_network_interface" "nic1" {
+    name                = var.name_nic1  
+    location            = azurerm_resource_group.rg.location
+    resource_group_name = azurerm_resource_group.rg.name
+
+    ip_configuration {
+    name                           = "config1"
+    subnet_id                      = azurerm_subnet.mySubnet.id 
+    private_ip_address_allocation  = "Static"
+    private_ip_address             = "10.0.0.10"
+    public_ip_address_id           = azurerm_public_ip.publicIP1.id
+    }
+    tags = {
+        environment = "CasoPractico2"
+    }
+
+}
+resource "azurerm_network_interface" "nic2" {
+    name                = var.name_nic2 
+    location            = azurerm_resource_group.rg.location
+    resource_group_name = azurerm_resource_group.rg.name
+
+    ip_configuration {
+    name                           = "config2"
+    subnet_id                      = azurerm_subnet.mySubnet.id 
+    private_ip_address_allocation  = "Static"
+    private_ip_address             = "10.0.0.11"
+    public_ip_address_id           = azurerm_public_ip.publicIP2.id
+    }
+    tags = {
+        environment = "CasoPractico2"
+    }
+
+}
+resource "azurerm_network_interface" "nic3" {
+    name                = var.name_nic3 
+    location            = azurerm_resource_group.rg.location
+    resource_group_name = azurerm_resource_group.rg.name
+
+    ip_configuration {
+    name                           = "config3"
+    subnet_id                      = azurerm_subnet.mySubnet.id 
+    private_ip_address_allocation  = "Static"
+    private_ip_address             = "10.0.0.12"
+    public_ip_address_id           = azurerm_public_ip.publicIP3.id
+    }
+    tags = {
+        environment = "CasoPractico2"
+    }
+
+}
+resource "azurerm_network_interface" "nic4" {
+    name                = var.name_nic4
+    location            = azurerm_resource_group.rg.location
+    resource_group_name = azurerm_resource_group.rg.name
+
+    ip_configuration {
+    name                           = "config4"
+    subnet_id                      = azurerm_subnet.mySubnet.id 
+    private_ip_address_allocation  = "Static"
+    private_ip_address             = "10.0.0.13"
+    public_ip_address_id           = azurerm_public_ip.publicIP4.id
+    }
+    tags = {
+        environment = "CasoPractico2"
+    }
+
+}
