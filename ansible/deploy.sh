42db158f9ca1fb15de2680f16234c81e02cf9c78
anog21 <106991230+anog21@users.noreply.github.com>
2022-07-24 00:26:53 +0200

    Delete ansible directory

--- ansible/deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-ansible-playbook 0-requirements-installation.yml -i hosts
-ansible-playbook 1-nfs-config.yml -i hosts
-ansible-playbook 2-master-config.yml -i hosts
-ansible-playbook 3-worker-config.yml -i hosts
